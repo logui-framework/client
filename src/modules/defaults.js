59e31dfa80ed1a1fb1a81baa67341853385a6bb9
David Maxwell <maxwelld90@gmail.com>
2021-03-04 22:54:26 +0000

    Removed old source code

--- src/modules/defaults.js
+++
@@ -1,6 +0,0 @@
-const logUIdefaults = {
-    verbose: true,  // Whether LogUI dumps events to console.log() or not.
-    sessionUUID: null, // The session UUID to be used (null means no previous UUID has been used).
-};
-
-export default logUIdefaults;
