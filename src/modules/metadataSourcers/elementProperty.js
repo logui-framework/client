3e2839ad3d5d0fe79b86354cd1f4f407b10749d8
David Maxwell <maxwelld90@gmail.com>
2022-02-03 14:19:16 +0000

    Updated elementProperty sourcer to be able to pull properties from another element

--- src/modules/metadataSourcers/elementProperty.js
+++ src/modules/metadataSourcers/elementProperty.js
@@ -9,6 +9,8 @@
     @date: 2021-03-25
 */
 
+import Helpers from '../helpers';
+
 export default (function(root) {
     var _sourcer = {};
 
@@ -34,6 +36,21 @@ export default (function(root) {
 
     _sourcer.getValue = function(element, request) {
         if (request.hasOwnProperty('lookFor')) {
+
+            if (request.hasOwnProperty('onElement')) {
+                let selectedElement = Helpers.$(request.onElement);
+
+                if (!selectedElement) {  // The element specified does not exist in the DOM.
+                    return;
+                }
+
+                if (selectedElement[request.lookFor]) {
+                    return selectedElement[request.lookFor];
+                }
+
+                return;
+            }
+            
             if (element[request.lookFor]) {
                 return element[request.lookFor];
             }
